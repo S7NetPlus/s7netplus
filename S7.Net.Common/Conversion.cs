4fd09e7db13eb74fc0ce9abf68650b8a8769caa3
Marcos <mdvp@osmtts.com>
2016-02-08 22:09:18 +0000

    VS2015 .Net Core compatible solution added.

---
+++ S7.Net.Common/Conversion.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Globalization;
+
+namespace S7.Net
+{
+    public static class Conversion
+    {
+        public static int BinStringToInt32(this string txt)
+        {
+            int cnt = 0;
+            int ret = 0;
+
+            for (cnt = txt.Length - 1; cnt >= 0; cnt += -1)
+            {
+                if (int.Parse(txt.Substring(cnt, 1)) == 1)
+                {
+                    ret += (int)(Math.Pow(2, (txt.Length - 1 - cnt)));
+                }
+            }
+            return ret;
+        }
+
+        public static byte? BinStringToByte(this string txt)
+        {
+            int cnt = 0;
+            int ret = 0;
+
+            if (txt.Length == 8)
+            {
+                for (cnt = 7; cnt >= 0; cnt += -1)
+                {
+                    if (int.Parse(txt.Substring(cnt, 1)) == 1)
+                    {
+                        ret += (int)(Math.Pow(2, (txt.Length - 1 - cnt)));
+                    }
+                }
+                return (byte)ret;
+            }
+            return null;
+        }
+
+        public static string ValToBinString(this object value)
+        {
+            int cnt = 0;
+            int cnt2 = 0;
+            int x = 0;
+            string txt = "";
+            long longValue = 0;
+
+            try
+            {
+                if (value.GetType().Name.IndexOf("[]") < 0)
+                {
+                    // ist nur ein Wert
+                    switch (value.GetType().Name)
+                    {
+                        case "Byte":
+                            x = 7;
+                            longValue = (long)((byte)value);
+                            break;
+                        case "Int16":
+                            x = 15;
+                            longValue = (long)((Int16)value);
+                            break;
+                        case "Int32":
+                            x = 31;
+                            longValue = (long)((Int32)value);
+                            break;
+                        case "Int64":
+                            x = 63;
+                            longValue = (long)((Int64)value);
+                            break;
+                        default:
+                            throw new Exception();
+                    }
+
+                    for (cnt = x; cnt >= 0; cnt += -1)
+                    {
+                        if (((Int64)longValue & (Int64)Math.Pow(2, cnt)) > 0)
+                            txt += "1";
+                        else
+                            txt += "0";
+                    }
+                }
+                else
+                {
+                    // ist ein Array
+                    switch (value.GetType().Name)
+                    {
+                        case "Byte[]":
+                            x = 7;
+                            byte[] ByteArr = (byte[])value;
+                            for (cnt2 = 0; cnt2 <= ByteArr.Length - 1; cnt2++)
+                            {
+                                for (cnt = x; cnt >= 0; cnt += -1)
+                                    if ((ByteArr[cnt2] & (byte)Math.Pow(2, cnt)) > 0) txt += "1"; else txt += "0";
+                            }
+                            break;
+                        case "Int16[]":
+                            x = 15;
+                            Int16[] Int16Arr = (Int16[])value;
+                            for (cnt2 = 0; cnt2 <= Int16Arr.Length - 1; cnt2++)
+                            {
+                                for (cnt = x; cnt >= 0; cnt += -1)
+                                    if ((Int16Arr[cnt2] & (byte)Math.Pow(2, cnt)) > 0) txt += "1"; else txt += "0";
+                            }
+                            break;
+                        case "Int32[]":
+                            x = 31;
+                            Int32[] Int32Arr = (Int32[])value;
+                            for (cnt2 = 0; cnt2 <= Int32Arr.Length - 1; cnt2++)
+                            {
+                                for (cnt = x; cnt >= 0; cnt += -1)
+                                    if ((Int32Arr[cnt2] & (byte)Math.Pow(2, cnt)) > 0) txt += "1"; else txt += "0";
+                            }
+                            break;
+                        case "Int64[]":
+                            x = 63;
+                            byte[] Int64Arr = (byte[])value;
+                            for (cnt2 = 0; cnt2 <= Int64Arr.Length - 1; cnt2++)
+                            {
+                                for (cnt = x; cnt >= 0; cnt += -1)
+                                    if ((Int64Arr[cnt2] & (byte)Math.Pow(2, cnt)) > 0) txt += "1"; else txt += "0";
+                            }
+                            break;
+                        default:
+                            throw new Exception();
+                    }
+                }
+                return txt;
+            }
+            catch 
+            {
+                return "";
+            }
+        }
+
+        public static bool SelectBit(this byte data, int bitPosition)
+        {
+            int mask = 1 << bitPosition;
+            int result = data & mask;
+
+            return (result != 0);
+        }
+
+        /// <summary>
+        /// Converts from ushort value to short value; it's used to retrieve negative values from words
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static short ConvertToShort(this ushort input)
+        {
+            short output;
+            output = short.Parse(input.ToString("X"), NumberStyles.HexNumber);
+            return output;
+        }
+
+        /// <summary>
+        /// Converts from short value to ushort value; it's used to pass negative values to DWs
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static ushort ConvertToUshort(this short input)
+        {
+            ushort output;
+            output = ushort.Parse(input.ToString("X"), NumberStyles.HexNumber);
+            return output;
+        }
+
+        /// <summary>
+        /// Converts from UInt32 value to Int32 value; it's used to retrieve negative values from DBDs
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static Int32 ConvertToInt(this uint input)
+        {
+            int output;
+            output = int.Parse(input.ToString("X"), NumberStyles.HexNumber);
+            return output;
+        }
+
+        /// <summary>
+        /// Converts from Int32 value to UInt32 value; it's used to pass negative values to DBDs
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static UInt32 ConvertToUInt(this int input)
+        {
+            uint output;
+            output = uint.Parse(input.ToString("X"), NumberStyles.HexNumber);
+            return output;
+        }
+
+        /// <summary>
+        /// Converts from double to DWord (DBD)
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static UInt32 ConvertToUInt(this double input) 
+        {
+            uint output;
+            output = S7.Net.Types.DWord.FromByteArray(S7.Net.Types.Double.ToByteArray(input));
+            return output;
+        }
+
+        /// <summary>
+        /// Converts from DWord (DBD) to double
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static double ConvertToDouble(this uint input)
+        {
+            double output;
+            output = S7.Net.Types.Double.FromByteArray(S7.Net.Types.DWord.ToByteArray(input));
+            return output;
+        }
+    }
+}
---
+++ S7.Net.Common/Types/Double.cs
@@ -0,0 +1,166 @@
+﻿using System;
+
+namespace S7.Net.Types
+{
+    public static class Double
+    {
+        // publics
+        #region FromByteArray
+        public static double FromByteArray(byte[] bytes)
+        {
+            byte v1 = bytes[0];
+            byte v2 = bytes[1];
+            byte v3 = bytes[2];
+            byte v4 = bytes[3];
+
+            if ((int)v1 + v2 + v3 + v4 == 0)
+            {
+                return 0.0;
+            }
+            else
+            {
+                // nun String bilden
+                string txt = ValToBinString(v1) + ValToBinString(v2) + ValToBinString(v3) + ValToBinString(v4);
+                // erstmal das Vorzeichen
+                int vz = int.Parse(txt.Substring(0, 1));
+                int exd = Conversion.BinStringToInt32(txt.Substring(1, 8));
+                string ma = txt.Substring(9, 23);
+                double mantisse = 1;
+                double faktor = 1.0;
+
+                //das ist die Anzahl der restlichen bit's
+                for (int cnt = 0; cnt <= 22; cnt++)
+                {
+                    faktor = faktor / 2.0;
+                    //entspricht 2^-y
+                    if (ma.Substring(cnt, 1) == "1")
+                    {
+                        mantisse = mantisse + faktor;
+                    }
+                }
+                return Math.Pow((-1), vz) * Math.Pow(2, (exd - 127)) * mantisse;
+            }
+        }
+        #endregion
+        #region FromDWord
+        public static double FromDWord(Int32 value)
+        {
+            byte[] b = DInt.ToByteArray(value);
+            double d = FromByteArray(b);
+            return d;
+        }
+
+        public static double FromDWord(UInt32 value)
+        {
+            byte[] b = DWord.ToByteArray(value);
+            double d = FromByteArray(b);
+            return d;
+        }
+        #endregion
+
+        #region ToByteArray
+        public static byte[] ToByteArray(double value)
+        {
+            double wert = (double)value;
+            string binString = "";
+            byte[] bytes = new byte[4];
+            if (wert != 0f)
+            {
+                if (wert < 0)
+                {
+                    wert *= -1;
+                    binString = "1";
+                }
+                else
+                {
+                    binString = "0";
+                }
+                int exponent = (int)Math.Floor((double)Math.Log(wert) / Math.Log(2.0));
+                wert = wert / (Math.Pow(2, exponent)) - 1;
+
+                binString += ValToBinString((byte)(exponent + 127));
+                for (int cnt = 1; cnt <= 23; cnt++)
+                {
+                    if (!(wert - System.Math.Pow(2, -cnt) < 0))
+                    {
+                        wert = wert - System.Math.Pow(2, -cnt);
+                        binString += "1";
+                    }
+                    else
+                        binString += "0";
+                }
+                bytes[0] = (byte)BinStringToByte(binString.Substring(0, 8));
+                bytes[1] = (byte)BinStringToByte(binString.Substring(8, 8));
+                bytes[2] = (byte)BinStringToByte(binString.Substring(16, 8));
+                bytes[3] = (byte)BinStringToByte(binString.Substring(24, 8));
+
+            }
+            else
+            {
+                bytes[0] = 0;
+                bytes[1] = 0;
+                bytes[2] = 0;
+                bytes[3] = 0;
+            }
+            return bytes;
+        }
+
+        public static byte[] ToByteArray(double[] value)
+        {
+            ByteArray arr = new ByteArray();
+            foreach (double val in value)
+                arr.Add(ToByteArray(val));
+            return arr.array;
+        }
+        #endregion
+        #region ToArray
+        public static double[] ToArray(byte[] bytes)
+        {
+            double[] values = new double[bytes.Length / 4];
+
+            int counter = 0;
+            for (int cnt = 0; cnt < bytes.Length / 4; cnt++)
+                values[cnt] = FromByteArray(new byte[] { bytes[counter++], bytes[counter++], bytes[counter++], bytes[counter++] });
+
+            return values;
+        }
+        #endregion
+        
+        // privates
+        #region ValToBinString
+        private static string ValToBinString(byte value)
+        {
+            string txt = "";
+
+            for (int cnt = 7; cnt >= 0; cnt += -1)
+            {
+                if ((value & (byte)Math.Pow(2, cnt)) > 0)
+                    txt += "1";
+                else
+                    txt += "0";
+            }
+            return txt;
+        }
+        #endregion
+        #region BinStringToByte
+        private static byte? BinStringToByte(string txt)
+        {
+            int cnt = 0;
+            int ret = 0;
+
+            if (txt.Length == 8)
+            {
+                for (cnt = 7; cnt >= 0; cnt += -1)
+                {
+                    if (int.Parse(txt.Substring(cnt, 1)) == 1)
+                    {
+                        ret += (int)(Math.Pow(2, (txt.Length - 1 - cnt)));
+                    }
+                }
+                return (byte)ret;
+            }
+            return null;
+        }
+        #endregion
+    }
+}
