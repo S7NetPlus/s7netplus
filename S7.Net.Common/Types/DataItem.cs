821d470e4b3bbd6d5fda24b008a217e2b5a42b36
Johnnyxy <30o_24ff4snfv2oz@byom.de>
2016-05-17 14:19:42 +0200

    x fixed VS2013 - C# 6 auto-property initializers - issue

--- S7.Net.Common/Types/DataItem.cs
+++ S7.Net.Common/Types/DataItem.cs
@@ -6,8 +6,13 @@
         public VarType VarType { get; set; }
         public int DB { get; set; }
         public int StartByteAdr { get; set; }
-        public int Count { get; set; } = 1;
+        public int Count { get; set; }
 
         public object Value { get; set; }
+
+        public DataItem()
+        {
+            Count = 1;
+        }
     }
 }
