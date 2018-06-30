909eb504eab87e38a5356a06b26fa19385a05144
Raphael <rs@sltgmbh.com>
2018-06-30 11:25:12 +0200

    Corrected NuGetPack.bat for create desired targets only

--- NuGetPack.bat
+++ NuGetPack.bat
@@ -1,12 +1,16 @@
 @echo off
-set /p version="Version: "
+REM Ask for Release version unless supplied by first commandline arg
+if "%1" == "" (
+	set /p version="Version: "
+) else (
+	set version=%1
+)
 msbuild S7.Net\S7.Net.csproj /P:Configuration=Release
-rmdir /S /Q nuget-pack\lib
-xcopy S7.Net\bin\Release\S7.Net.dll nuget-pack\lib\net35\ /Y 
-xcopy S7.Net\bin\Release\S7.Net.xml nuget-pack\lib\net35\ /Y 
-xcopy S7.Net\bin\Release\S7.Net.dll nuget-pack\lib\net40\ /Y 
-xcopy S7.Net\bin\Release\S7.Net.xml nuget-pack\lib\net40\ /Y 
-xcopy S7.Net\bin\Release\S7.Net.dll nuget-pack\lib\net45\ /Y 
-xcopy S7.Net\bin\Release\S7.Net.xml nuget-pack\lib\net45\ /Y 
+rmdir /S /Q nuget-pack\lib\%version%
+xcopy S7.Net\bin\Release\net452\S7.Net.dll nuget-pack\lib\%version%\net452\ /Y 
+xcopy S7.Net\bin\Release\net452\S7.Net.xml nuget-pack\lib\%version%\net452\ /Y 
+xcopy S7.Net\bin\Release\netstandard2.0\S7.Net.dll nuget-pack\lib\%version%\netstandard2.0\ /Y 
+xcopy S7.Net\bin\Release\netstandard2.0\S7.Net.xml nuget-pack\lib\%version%\netstandard2.0\ /Y 
 .nuget\nuget pack nuget-pack\S7.Net.nuspec -Version %version%
+move S7netplus.%version%.nupkg nuget-pack\lib\%version%\
 pause
