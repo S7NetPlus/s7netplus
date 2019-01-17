f96d2f049866a30b267bec9aef6d1fc0d4cbeee0
xubin <xub@skeqi.com>
2019-01-17 15:36:38 +0800

    添加winform项目

---
+++ S7.Net.Test/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace S7.Net.Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
