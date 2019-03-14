4ae905ffd54ddb7b761a085857d11117adf6ebdb
Michael Croes <mycroes@gmail.com>
2019-03-14 22:25:25 +0100

    Set PDU size to 960
    Allows communication with Siemens Logo 0BA8, currently no Siemens PLC
    is known to support >960 PDU size.

--- S7.Net/Protocol/ConnectionRequest.cs
+++ S7.Net/Protocol/ConnectionRequest.cs
@@ -21,7 +21,7 @@ namespace S7.Net.Protocol
                     3, 0,   //Destination TASP
                     192,    //Parameter Code (tpdu-size)
                     1,      //Parameter Length
-                    11      //TPDU Size (2^11 = 2048)
+                    10      //TPDU Size (2^10 = 1024)
                 };
 
             switch (cpu)
