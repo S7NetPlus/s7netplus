71cd7eb7fda5cfc4f37bdbd6d9980997f31b901c
Tim Verwaal <timverwaal@gmail.com>
2019-12-19 09:22:14 +0100

    Added test for DataType Memory and varType Byte

--- S7.Net.UnitTest/PLCAddressParsingTests.cs
+++ S7.Net.UnitTest/PLCAddressParsingTests.cs
@@ -12,14 +12,23 @@ namespace S7.Net.UnitTest
         {
             DataItem dataItem = DataItem.FromAddress("M2000.1");
 
-            Assert.AreEqual(dataItem.DataType, DataType.Memory, "Wrong datatype for M2000.1");
-            Assert.AreEqual(dataItem.DB , 0, "Wrong dbnumber for M2000.1");
-            Assert.AreEqual(dataItem.VarType, VarType.Bit, "Wrong vartype for M2000.1");
-            Assert.AreEqual(dataItem.StartByteAdr, 2000, "Wrong startbyte for M2000.1");
-            Assert.AreEqual(dataItem.BitAdr, 1, "Wrong bit for M2000.1");
-
-            
+            Assert.AreEqual(DataType.Memory, dataItem.DataType, "Wrong datatype for M2000.1");
+            Assert.AreEqual(0, dataItem.DB, "Wrong dbnumber for M2000.1");
+            Assert.AreEqual(VarType.Bit, dataItem.VarType, "Wrong vartype for M2000.1");
+            Assert.AreEqual(2000, dataItem.StartByteAdr, "Wrong startbyte for M2000.1");
+            Assert.AreEqual(1, dataItem.BitAdr, "Wrong bit for M2000.1");
         }
 
+        [TestMethod]
+        public void T02_ParseMB200()
+        {
+            DataItem dataItem = DataItem.FromAddress("MB200");
+
+            Assert.AreEqual(DataType.Memory, dataItem.DataType, "Wrong datatype for MB200");
+            Assert.AreEqual(0, dataItem.DB, "Wrong dbnumber for MB200");
+            Assert.AreEqual(VarType.Byte, dataItem.VarType, "Wrong vartype for MB200");
+            Assert.AreEqual(200, dataItem.StartByteAdr, "Wrong startbyte for MB200");
+            Assert.AreEqual(0, dataItem.BitAdr, "Wrong bit for MB200");
+        }
     }
 }
