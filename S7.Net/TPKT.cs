10e556270661bc9f26790236ec9a572907ff5a85
Jakob Ledermann <jakob.ledermann@steinbach-gruppe.de>
2020-07-17 11:30:28 +0200

    Read desired amount from stream. see #273
    The read method on a networkstream blocks only until the first byte is available.
    It is not guaranteed to read the desired count into the buffer.
    
    This solution tries to read the remaining bytes in a loop and aborts once the
    full count is read or the Stream.Read method returns with 0 or less bytes read.
    
    The synchronous read can block indefinitly if the lenght field is larger than the send package.

--- S7.Net/TPKT.cs
+++ S7.Net/TPKT.cs
@@ -32,21 +32,16 @@ namespace S7.Net
         public static TPKT Read(Stream stream)
         {
             var buf = new byte[4];
-            int len = stream.Read(buf, 0, 4);
-            if (len < 4) throw new TPKTInvalidException("TPKT header incomplete / invalid");
+            int len = stream.ReadFixed(buf, 0, 4);
+            if (len < 4) throw new TPKTInvalidException("TPKT is incomplete / invalid");
             var version = buf[0];
             var reserved1 = buf[1];
             var length = buf[2] * 256 + buf[3]; //BigEndian
 
-            if (length == 0)
-            {
-                throw new TPKTInvalidException("TPKT payload length is zero");
-            }
-
             var data = new byte[length - 4];
-            len = stream.Read(data, 0, length - 4);
-            if (len < length - 4)
-                throw new TPKTInvalidException("TPKT payload incomplete / invalid");
+            len = stream.ReadFixed(data, 0, data.Length);
+            if (len < data.Length)
+                throw new TPKTInvalidException("TPKT is incomplete / invalid");
 
             return new TPKT
             (
@@ -65,20 +60,16 @@ namespace S7.Net
         public static async Task<TPKT> ReadAsync(Stream stream)
         {
             var buf = new byte[4];
-            int len = await stream.ReadAsync(buf, 0, 4);
-            if (len < 4) throw new TPKTInvalidException("TPKT header incomplete / invalid");
+            int len = await stream.ReadFixedAsync(buf, 0, 4);
+            if (len < 4) throw new TPKTInvalidException("TPKT is incomplete / invalid");
+
             var version = buf[0];
             var reserved1 = buf[1];
             var length = buf[2] * 256 + buf[3]; //BigEndian
 
-            if (length == 0)
-            {
-                throw new TPKTInvalidException("TPKT payload length is zero");
-            }
-
             var data = new byte[length - 4];
-            len = await stream.ReadAsync(data, 0, length - 4);
-            if (len < length - 4)
+            len = await stream.ReadFixedAsync(data, 0, data.Length);
+            if (len < data.Length)
                 throw new TPKTInvalidException("TPKT payload incomplete / invalid");
 
             return new TPKT
