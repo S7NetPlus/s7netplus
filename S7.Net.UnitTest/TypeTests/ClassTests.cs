9a34b14e1ea45818360d5a7568a5f5627e4436c9
Michael Croes <mycroes@gmail.com>
2020-08-13 21:57:42 +0200

    Tests/TypeTests: Add ClassTests from #178
    Close #178.

---
+++ S7.Net.UnitTest/TypeTests/ClassTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using S7.Net.Types;
+
+namespace S7.Net.UnitTest.TypeTests
+{
+    [TestClass]
+    public class ClassTests
+    {
+        [TestMethod]
+        public void GetClassSizeTest()
+        {
+            Assert.AreEqual(Class.GetClassSize(new TestClassUnevenSize(1, 1)), 6);
+            Assert.AreEqual(Class.GetClassSize(new TestClassUnevenSize(2, 15)), 6);
+            Assert.AreEqual(Class.GetClassSize(new TestClassUnevenSize(2, 16)), 6);
+            Assert.AreEqual(Class.GetClassSize(new TestClassUnevenSize(2, 17)), 8);
+            Assert.AreEqual(Class.GetClassSize(new TestClassUnevenSize(3, 15)), 8);
+            Assert.AreEqual(Class.GetClassSize(new TestClassUnevenSize(3, 17)), 10);
+        }
+
+        private class TestClassUnevenSize
+        {
+            public bool Bool { get; set; }
+            public byte[] Bytes { get; set; }
+            public bool[] Bools { get; set; }
+
+            public TestClassUnevenSize(int byteCount, int bitCount)
+            {
+                Bytes = new byte[byteCount];
+                Bools = new bool[bitCount];
+            }
+        }
+    }
+}
