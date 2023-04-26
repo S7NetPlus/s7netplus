cc7e34ff67fbbfe451f44c021d63d55c12a1d2bc
TobyStoe <97510125+TobyStoe@users.noreply.github.com>
2023-04-26 09:12:22 +0200

    Create sByte.cs

---
+++ S7.Net/Types/sByte.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace S7.Net.Types
+{
+    /// <summary>
+    /// Contains the methods to convert from bytes to byte arrays
+    /// </summary>
+    public static class sByte
+    {
+        /// <summary>
+        /// Converts a byte to byte array
+        /// </summary>
+        public static byte[] ToByteArray(byte value)
+        {
+            return new byte[] { value }; ;
+        }
+       
+        /// <summary>
+        /// Converts a byte array to byte
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        public static byte FromByteArray(byte[] bytes)
+        {
+            if (bytes.Length != 1)
+            {
+                throw new ArgumentException("Wrong number of bytes. Bytes array must contain 1 bytes.");
+            }
+            return bytes[0];
+        }
+        
+    }
+}
